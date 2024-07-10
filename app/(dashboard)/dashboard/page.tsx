924390679ace2f078001f285e3b36ba34467af11
Georgey <68821192+GeoBrodas@users.noreply.github.com>
2024-07-10 22:32:12 +0530

    build dashboard chat layout

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,87 @@
-import { Metadata } from 'next';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'Genkin -Dashboard',
-  // description: '',
-};
+import { useUIState } from 'ai/rsc';
+import { AI } from '../actions';
+import { useState } from 'react';
 
-export default function DashboardPage() {}
+export default function DashboardPage() {
+  const [input, setInput] = useState<string>('');
+  const [idle, setIsNotIdle] = useState<boolean>(true);
+  const [conversation, setConversation] = useUIState<typeof AI>();
+
+  // chat submit action
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    if (idle) {
+      setIsNotIdle(false);
+    }
+  };
+
+  return idle ? (
+    <IdleDisplayChat
+      handleSubmit={handleSubmit}
+      input={input}
+      setInput={setInput}
+    />
+  ) : (
+    <ActiveChat
+      handleSubmit={handleSubmit}
+      conversation={conversation}
+      input={input}
+      setInput={setInput}
+    />
+  );
+}
+
+function IdleDisplayChat({ handleSubmit, input, setInput }) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col w-full max-w-xl py-24 mx-auto stretch">
+        <div className="w-full">hi</div>
+
+        <ChatInput
+          handleSubmit={handleSubmit}
+          input={input}
+          setInput={setInput}
+        />
+      </div>
+    </main>
+  );
+}
+
+function ActiveChat({ conversation, handleSubmit, input, setInput }) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col w-full max-w-xl py-24 mx-auto stretch">
+        <div className="space-y-4">
+          {conversation.map((m: any) => (
+            <div key={m.id} className="whitespace-pre-wrap">
+              <span className="font-bold">
+                {m.role === 'user' ? 'User: ' : 'AI: '}
+              </span>
+              <span>{m.content}</span>
+            </div>
+          ))}
+        </div>
+
+        <ChatInput
+          handleSubmit={handleSubmit}
+          input={input}
+          setInput={setInput}
+        />
+      </div>
+    </main>
+  );
+}
+
+function ChatInput({ handleSubmit, input, setInput }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="fixed text-black bottom-0 w-full max-w-xl p-4 mb-8 border border-red-300 rounded-full focus:outline-none"
+        value={input}
+        placeholder="Type your prompt"
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </form>
+  );
+}
---
+++ lib/types.ts
@@ -0,0 +1,20 @@
+export interface Chat extends Record<string, any> {
+  id: string;
+  title: string;
+  createdAt: Date;
+  userId: string;
+  path: string;
+  messages: Message[];
+  sharePath?: string;
+}
+
+export type Message = {
+  role: 'user' | 'assistant' | 'system' | 'function' | 'data' | 'tool';
+  content: string;
+  id?: string;
+  name?: string;
+  display?: {
+    name: string;
+    props: Record<string, any>;
+  };
+};
