ee344bc5262076736d00dd7ffc7a9e6de2266e6d
Georgey <68821192+GeoBrodas@users.noreply.github.com>
2024-07-11 22:33:31 +0530

    complete basic skeleton for idle chat

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import { useUIState } from 'ai/rsc';
 import { AI } from '../actions';
 import { useState } from 'react';
+import { SendHorizontal } from 'lucide-react';
 
 export default function DashboardPage() {
   const [input, setInput] = useState<string>('');
@@ -34,16 +35,39 @@ export default function DashboardPage() {
 
 function IdleDisplayChat({ handleSubmit, input, setInput }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col w-full max-w-xl py-24 mx-auto stretch">
-        <div className="w-full"></div>
+    <main className="flex flex-col items-center pt-16">
+      <div className="flex flex-col h-[34rem] justify-between w-full max-w-2xl pt-10 mx-auto stretch">
+        <div className="w-full">
+          <h3 className="text-4xl font-semibold">Genkin AI Chat system</h3>
 
-        <ChatInput
-          isFixed={false}
-          handleSubmit={handleSubmit}
-          input={input}
-          setInput={setInput}
-        />
+          <p className="mt-2">
+            Genkin leverages the power of Google Gemini and Vercel AI SDK to
+            give intelligent repsonses to your questions. Go ahead and try
+            <span className="italic">
+              &quot;Made and investment of $400 today&quot;
+            </span>
+          </p>
+        </div>
+
+        <div className="flex flex-col space-y-4">
+          <div className="grid gap-4 grid-cols-2">
+            <div className="bg-rose-50 hover:bg-rose-100 transition duration-100 ease-in-out rounded-lg p-4 hover:cursor-pointer">
+              <h5 className="font-semibold">List all trasactions</h5>
+              <p>from the past 20 days</p>
+            </div>
+            <div className="bg-rose-50 hover:bg-rose-100 transition duration-100 ease-in-out rounded-lg p-4 hover:cursor-pointer">
+              <h5 className="font-semibold">How much did I spend</h5>
+              <p>on fuel this month?</p>
+            </div>
+          </div>
+
+          <ChatInput
+            isFixed={false}
+            handleSubmit={handleSubmit}
+            input={input}
+            setInput={setInput}
+          />
+        </div>
       </div>
     </main>
   );
@@ -51,8 +75,8 @@ function IdleDisplayChat({ handleSubmit, input, setInput }) {
 
 function ActiveChat({ conversation, handleSubmit, input, setInput }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col w-full max-w-xl py-24 mx-auto stretch">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-16">
+      <div className="flex flex-col w-full max-w-2xl pt-10 mx-auto stretch">
         <div className="space-y-4">
           {conversation.map((m: any) => (
             <div key={m.id} className="whitespace-pre-wrap">
@@ -77,15 +101,19 @@ function ActiveChat({ conversation, handleSubmit, input, setInput }) {
 
 function ChatInput({ handleSubmit, input, setInput, isFixed }) {
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className={`${
+        isFixed && 'fixed bottom-11'
+      } flex bg-rose-50 justify-between rounded-full w-full max-w-2xl items-center`}
+    >
       <input
-        className={`${
-          isFixed && 'fixed bottom-0'
-        } text-black w-full max-w-xl p-4 mb-8 border border-red-300 rounded-full focus:outline-none`}
+        className={`text-black bg-rose-50 w-full rounded-l-full max-w-2xl p-4 focus:outline-none`}
         value={input}
         placeholder="Type your prompt"
         onChange={(e) => setInput(e.target.value)}
       />
+      <SendHorizontal className="relative text-red-600 z-10 right-4" />
     </form>
   );
 }
