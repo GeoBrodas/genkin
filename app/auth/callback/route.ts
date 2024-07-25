de9b108c5b0263f4ca182436fcd139026504d862
Georgey <68821192+GeoBrodas@users.noreply.github.com>
2024-07-25 06:55:47 +0530

    deploy fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,6 +8,8 @@ export async function GET(request: Request) {
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/dashboard';
 
+  console.log(searchParams, origin);
+
   if (code) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
